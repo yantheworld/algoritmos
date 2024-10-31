71a38029d62e48e8ab07e11caea32aed0d58f2b5
Carlos Thayan <thayanmoreira1@gmail.com>
2024-10-31 19:35:29 -0300

    enviando exercicios

---
+++ ex002.py
@@ -0,0 +1,4 @@
+#Faça o um programa que leia o nome de uma pessoa e mostre a mensagem de boas vindas.
+nome = input('Digite seu nome: ')
+print('é um prazer te conhecer, ', nome)
+print('É um prazer te conhecer, {}!'.format(nome))
---
+++ ex003.py
@@ -0,0 +1,17 @@
+
+#Desafios aula teste
+#Crie um programa que leia dois numeros e mostre a soma entre eles.
+
+n1 = float(input('Digite o primeiro valor: '))
+n2 = float(input('Digite o segundo valor: '))
+
+soma = (n1+n2)
+
+print('A soma dos valores é igual a: ', soma)
+
+
+#Testando os tipos em python
+#coisa = input('Digite qualquer coisa: ')
+
+#print(coisa)
+#print(type(coisa))
